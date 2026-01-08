bb6b858310eb4628cd2261f7de68401e41d49387
itsrasika855-tech <itsrasika855@gmail.com>
2026-01-09 00:02:37 +0530

    Added SE subjects folder

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".read-more").addEventListener("click", function () {
+    alert("You clicked Read More!");
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
